268ef291325136c1db38da722e651722550a4e4e
aadamcz <33779500+aadamcz@users.noreply.github.com>
2017-12-02 13:31:15 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function drawTree(n) {
-	for (var i = 1; i <= n; i++){
-  		var star = ''
-  		for (var j = 1; j <= i; j++){
+	for (var i = 1; i <= n; i++) {
+  		var star = '';
+  		for (var j = 1; j <= i; j++) {
     			star = star + '*';
-  			};
+  		};
   		console.log(star);
 	};
 };	
